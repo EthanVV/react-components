d5281b45f21d49458a40d71dbd0d30496735c502
EthanVV <ethan.vanvleck@gmail.com>
2018-10-01 15:48:04 -0500

    first functional section

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <li key ={this.props.todo}>{this.props.todo}</li>;
+  }
+}
+
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo => 
+      <TodoListItem todo = {todo} />
+    )}
+  </ul>
+)
+
+class GroceryListItem() {
+  
+}
+
+var todoList = new TodoList({todos: ['test', 'anotherTest']});
+
+ReactDOM.render(todoList, document.getElementById('app'));
