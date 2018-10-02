7cef6bf17d00ef9bf578fc7d70d4084aacd60382
EthanVV <ethan.vanvleck@gmail.com>
2018-10-01 21:58:04 -0500

    add line through list items on mouse over

--- app.js
+++ app.js
@@ -2,9 +2,22 @@
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {done: false};
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
   }
   render() {
-    return <li>{this.props.groceries}</li>
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>
+        {this.props.groceries}
+      </li>
+    );
   }
 }
 
@@ -24,7 +37,7 @@ let App = () => (
   <div>
     {GroceryList(test)}
   </div>
-)
+);
 
 ReactDOM.render(
   <ul><App /></ul>,
